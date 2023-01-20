858cb962ed9afa93bf21f742380a3e6d8a9851c4
Latada <icom92@naver.com>
2023-01-20 11:57:05 +0900

    간단 작동법 및 예제<랜덤명언>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const proverbs = [
+    "모두에게 전성기가 있지만 어떤 이들의 전성기는 다른 이들보다 더 길다.",
+    "싸우지 않고 적이 스스로 항복하는 것이 최고의 승리이다... 싸우지 않고 이기는 것이 최선이다.",
+    "성공이란 열정을 잃지 않고 실패를 거듭할 수 있는 능력이다.", 
+    "변명 중에서도 가장 어리석고 못난 변명은 '시간이 없어서'라는 변명이다."
+  ]
+  const getRandomIndex = value => {
+    return parseInt(Math.random() * value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +20,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {proverbs[getRandomIndex(proverbs.length)]}
       </header>
     </div>
   );
